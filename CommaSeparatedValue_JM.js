2930a45a5bdf3944b87f110c661f2089f7a29907
juanma3108 <alu0100597929@ull.edu.es>
2014-02-13 23:52:29 +0000

    añadido codigo de calculo de tamaño comun

--- CommaSeparatedValue_JM.js
+++ CommaSeparatedValue_JM.js
@@ -13,8 +13,9 @@ function tablaCSV()
   var datos = document.getElementById("datosIN").value;
   var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
   var lineas = datos.split(/\n+\s*/);
-  var tamFilaComun = NaN;
   var r = [];
+  var i;
+  var j;
 
   
   if (window.localStorage)
@@ -22,8 +23,82 @@ function tablaCSV()
           localStorage.datosIN  = datos;
       }
 
+  /*=========================================================
+    El siguiente codigo se encarga de calcular el tamaño de filas mas comun.
 
-  for(var i = 0; i < lineas.length; i++)
+    En la implementacion anterior, el tamaño de fila por defecto se asignaba al
+    tamaño de la primera fila, de tal forma que el tamaño de la primera fila era
+    el correcto, por tanto el tamaño de las siguientes filas era correcto si era 
+    IGUAL al de la primera fila, en caso contrario era erroneo.
+
+    El siguiente codigo compara el tamaño de todas las filas e interpreta como 
+    correcto el tamaño mas comun entra las filas, independientemente de su posicion 
+    en la tabla.
+  =========================================================*/
+  var tam = new Array(lineas.length);           //Array tam = almacena el tamaño de cada fila            
+  var comp = true;
+
+  for(i = 0; i < lineas.length; i++)
+  {
+      var elementos = lineas[i].match(regexp);
+      tam[i] = elementos.length;
+  }
+
+
+  var repet = new Array(tam.length);          //Array repet = almacena el numero de veces que se repite el tamaño
+  var errorTam = new Array(tam.length);       //Array errorTam = almacena false si el tamaño es el mas comun, true en caso contrario
+  var num;
+  var repetido;
+
+  for(i = 0; i < tam.length; i++)
+  {
+    num = tam[i];
+    repetido = 1;
+
+    for(j = 0; j < tam.length; j++)
+    {
+      if(j != i)
+      {
+        if(tam[j] == num)
+          {
+            repetido++;
+            repet[i] = repetido;
+          }
+          else
+            {
+              repet[i] = repetido;
+            }
+      }
+    }
+  }
+
+  var max = repet[0];
+
+  for(i = 0; i < tam.length; i++)
+    { 
+      if(repet[i] > max)
+      {
+        max = repet[i];
+      }
+    }
+
+    for(i = 0; i < tam.length; i++) 
+    { 
+      if(repet[i] == max)
+      {
+        errorTam[i] = false;
+      }
+      else
+        {
+          errorTam[i] = true;
+        }
+        
+    }
+
+  /*=========================================================*/
+
+
+  for(i = 0; i < lineas.length; i++)
   {
        var elementos = lineas[i].match(regexp);
        var resultado = [];
@@ -31,15 +106,8 @@ function tablaCSV()
 
         if(elementos)
         {
-            if (tamFilaComun && (tamFilaComun != elementos.length)) 
-            {
-              error = true;
-            }
-            else 
-            {
-              tamFilaComun = elementos.length;
-              error = false;
-            }
+
+            error = errorTam[i];
 
             for(var j = 0; j < elementos.length; j++)
             {
@@ -58,11 +126,6 @@ function tablaCSV()
             alert("Los datos introducidos no pertenecen al formato CSV");
             error = true;
         }
-      
-      
-      /*r.unshift('<p>\n<table class="center" id="result">');
-      r.push('</table>');
-      tablaResultados.innerHTML = r.join('\n');*/
 
   }
 
